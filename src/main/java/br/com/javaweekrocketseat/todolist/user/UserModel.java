14f7df983ebe3a79259f1c3e0396846a530ad7ea
Felipe Basilio <felipe.basilio@avenuecode.com>
2023-10-09 15:30:46 -0300

    [INIT]: Created first commit and developed first class assignment

---
+++ src/main/java/br/com/javaweekrocketseat/todolist/user/UserModel.java
@@ -0,0 +1,37 @@
+package br.com.javaweekrocketseat.todolist.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+
+    public UserModel(String username, String name, String password) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
